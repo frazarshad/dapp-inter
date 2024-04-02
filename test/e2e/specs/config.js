266a6b9e844a0f2cf7af63a8aea3f88cd337fab2
Fraz Arshad <fraz.arshad12@gmail.com>
2024-04-02 14:03:25 +0500

    test: using better selectors

---
+++ test/e2e/specs/config.js
@@ -0,0 +1,12 @@
+export const MINUTE_MS = 60000;
+
+export const phrasesList = {
+  emerynet: {
+    interNetwork: 'Agoric Emerynet',
+    isLocal: false,
+  },
+  local: {
+    interNetwork: 'Local Network',
+    isLocal: true,
+  },
+};
